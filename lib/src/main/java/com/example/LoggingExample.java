544fe04cd83bf9c4757838d54dfe204804e467c7
Asritha Kuruguntla <asritha.kuruguntla@gmail.com>
2025-11-08 10:20:03 +0530

    Logging App complete code

---
+++ lib/src/main/java/com/example/LoggingExample.java
@@ -0,0 +1,20 @@
+package com.example;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingExample {
+    private static final Logger logger = LogManager.getLogger(LoggingExample.class);
+
+    public static void main(String[] args) {
+        logger.info("App started successfully");
+        try {
+            int result = 10 / 0;
+        } catch (Exception e) {
+            logger.error("Division failed!", e);
+        }
+        logger.warn("This is a warning message");
+        logger.info("App finished");
+    }
+}
